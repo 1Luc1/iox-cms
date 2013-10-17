5b9e7e97708c58b9444f129980ca1425fd6365d4
Thorsten Zerha <thorsten.zerha@tastenwerk.com>
2013-10-17 18:35:19 +0200

    devices helper showing mobile device information

---
+++ app/assets/javascripts/iox/devices_helper.js
@@ -0,0 +1,14 @@
+/**
+ * iox.devices helper
+ */
+
+(function(){
+	
+	'use strict';
+
+	window.iox = window.iox || {};
+	window.iox.devices = window.iox.devices || {};
+
+	window.iox.devices.isMobile = false;
+
+})();

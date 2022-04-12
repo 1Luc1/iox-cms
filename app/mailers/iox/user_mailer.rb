039d43dd1e85ecd223f95f2933ae0e5e658a7282
David Reinisch <david@k-os.online>
2022-04-12 13:34:57 +0200

    https fixes

--- app/mailers/iox/user_mailer.rb
+++ app/mailers/iox/user_mailer.rb
@@ -6,7 +6,7 @@ module Iox
     def welcome_email(user, creator)
       @user = user
       @creator = creator
-      @url  = "http://#{Rails.configuration.iox.domain_name}/iox/welcome/#{user.id}?k=#{user.confirmation_key}"
+      @url  = "https://#{Rails.configuration.iox.domain_name}/iox/welcome/#{user.id}?k=#{user.confirmation_key}"
       @support_email = Rails.configuration.iox.support_email
       @site_title = Rails.configuration.iox.site_title
       mail( to: @user.email, subject: "[#{Rails.configuration.iox.site_title}] #{I18n.t('user.mailer.subject')}" )
@@ -14,20 +14,20 @@ module Iox
 
     def registration_welcome_email(user)
       @user = user
-      @url  = "http://#{Rails.configuration.iox.domain_name}/iox/welcome/#{user.id}?k=#{user.confirmation_key}"
+      @url  = "https://#{Rails.configuration.iox.domain_name}/iox/welcome/#{user.id}?k=#{user.confirmation_key}"
       @site_title = Rails.configuration.iox.site_title
       mail( to: @user.email, bcc: admin_emails, subject: "[#{Rails.configuration.iox.site_title}] #{I18n.t('user.registration_form.subject')}" )
     end
 
     def confirmation_email(user)
       @user = user
-      @url  = "http://#{Rails.configuration.iox.domain_name}/iox/confirm/#{user.confirmation_key}"
+      @url  = "https://#{Rails.configuration.iox.domain_name}/iox/confirm/#{user.confirmation_key}"
       mail( to: @user.email, subject: "[#{Rails.configuration.iox.site_title}] #{I18n.t('user.mailer.welcome')}" )
     end
 
     def forgot_password(user)
       @user = user
-      @url = "http://#{Rails.configuration.iox.domain_name}/iox/reset_password/#{user.id}?k=#{user.confirmation_key}"
+      @url = "https://#{Rails.configuration.iox.domain_name}/iox/reset_password/#{user.id}?k=#{user.confirmation_key}"
       mail( to: @user.email, subject: "[#{Rails.configuration.iox.site_title}] #{I18n.t('auth.forgot_password_request')}" )
     end
 

51b521f8878c140320afbb724179e95b6099ae09
1Luc1 <lukas.aigner@gmail.com>
2024-10-28 19:12:07 +0100

    update copy public folder generator

--- lib/generators/iox/copy_public_folder/copy_public_folder_generator.rb
+++ lib/generators/iox/copy_public_folder/copy_public_folder_generator.rb
@@ -4,12 +4,15 @@ module Iox
   module Generators
     class CopyPublicFolderGenerator < Rails::Generators::Base
 
-      source_root File.expand_path('../templates', __FILE__)
+      source_root File.expand_path('../../install/templates', __FILE__)
 
       desc "Copy public folder files"
 
       def setup_and_create    
-        directory "images", "public/images"
+        directory "webfonts", "public/webfonts"
+        directory "javascripts", "public/javascripts/3rdparty/"
+        directory "avatar", "public/images/iox/avatar"
+        directory "kendoui", "public/stylesheets/3rdparty/kendoui"
       end
     end
   end
